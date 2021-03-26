2fd91bde7e72a533056ee06394105039635ee21d
Lina Huertas <linamarcelahuertas7@gmail.com>
2021-03-25 21:05:47 -0500

    Seunificaron los textos

--- Azul.js
+++ Azul.js
@@ -1,8 +1,6 @@
 console.log("EL COLOR DEL MAR");
 console.log(" EL MEJOR");
-<<<<<<< HEAD
 
-console.log ("el color azul es el color de la tranquilidad y la paz")
-=======
-console.log("El mar con los peces es azul");
->>>>>>> 5cfb35927a8e35d45b7d372f683049b0cb3fa764
+console.log ("el color azul es el color de la tranquilidad y la paz y con los peces es azul");
+
+
